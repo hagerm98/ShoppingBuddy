0d910e903ed7e5670850c16637bedaff0094773b
Hager Khamis <hagerm98@hotmail.com>
2025-07-06 22:30:45 +0100

    Initial Spring Project Files

---
+++ src/main/java/com/hager/shoppingbuddy/ShoppingbuddyApplication.java
@@ -0,0 +1,13 @@
+package com.hager.shoppingbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingbuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingbuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hager/shoppingbuddy/ShoppingbuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hager.shoppingbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingbuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

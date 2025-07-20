eaa96cc091777075d1b1dc55039de22e66565cd1
Hager Khamis <hagerm98@hotmail.com>
2025-07-20 05:01:55 +0100

    Added custom exceptions, and an error generic page. Added a favicon.ico file to avoid errors from browser requesting icon

---
+++ src/main/java/com/hager/shoppingbuddy/exception/ShoppingBuddyException.java
@@ -0,0 +1,7 @@
+package com.hager.shoppingbuddy.exception;
+
+import lombok.experimental.StandardException;
+
+@StandardException
+public abstract class ShoppingBuddyException extends Exception {
+}
